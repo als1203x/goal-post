98270d355e6c46fd8a75304f06f15e0c42e1c13a
LinuxPlus <linuxplus@linuxpluss-mac.local>
2018-01-26 09:04:35 -0800

    Create custom transition animation from GoalVC to CreateGoalVC & vice versa

--- goal post/Controller/GoalsVC.swift
+++ goal post/Controller/GoalsVC.swift
@@ -22,7 +22,10 @@ class GoalsVC: UIViewController {
     
     
     @IBAction func addGoalBtnPressed(_ sender: Any) {
-   
+        
+        guard let createGoalVC = storyboard?.instantiateViewController(withIdentifier: "CreateGoalVC") else { return }
+        
+        presentDetail(createGoalVC)
     }
 }
 
---
+++ goal post/Extensions/UIViewControllerExt.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewControllerExt.swift
+//  goal post
+//
+//  Created by LinuxPlus on 1/26/18.
+//  Copyright © 2018 ARC. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController  {
+        //present viewController by changing transition type
+    func presentDetail(_ viewControllerToPresent: UIViewController) {
+        let transition = CATransition()
+        transition.duration = 0.3
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromRight
+        self.view.window?.layer.add(transition, forKey: kCATransition)
+        
+        present(viewControllerToPresent, animated: false, completion: nil)
+    }
+    
+    func dismissDetail()    {
+        let transition = CATransition()
+        transition.duration = 0.3
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromLeft
+        self.view.window?.layer.add(transition, forKey: kCATransition)
+        
+        dismiss(animated: false, completion: nil)
+    }
+}
