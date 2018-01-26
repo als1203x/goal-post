a34045c7672772cf43d58b93063af8b7b2df9ddb
LinuxPlus <linuxplus@linuxpluss-mac.local>
2018-01-26 12:03:09 -0800

    Save Goal to persistent Store

--- goal post/Controller/CreateGoalVC.swift
+++ goal post/Controller/CreateGoalVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CreateGoalVC: UIViewController {
+class CreateGoalVC: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var goalTextView: UITextView!
     @IBOutlet weak var shortTermBtn: UIButton!
@@ -21,6 +21,7 @@ class CreateGoalVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         nextBtn.bindToKeyboard()
+        goalTextView.delegate = self
         
         shortTermBtn.setSelectorColor()
         longTermBtn.setDeselectedColor()
@@ -38,9 +39,21 @@ class CreateGoalVC: UIViewController {
     }
     
     @IBAction func nextBtnPressed(_ sender: Any) {
+        if goalTextView.text != "" && goalTextView.text != "What is your goal?" {
+            guard let finishGoalVC = storyboard?.instantiateViewController(withIdentifier: "FinishGoalVC") as? FinishGoalVC else { return }
+            finishGoalVC.initData(description: goalTextView.text!, type: goalType )
+            presentDetail(finishGoalVC)
+        }
     }
+    
+    
     @IBAction func backBtnPressed(_ sender: Any) {
             dismissDetail()
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        goalTextView.text = ""
+        goalTextView.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
     }
     
 }
---
+++ goal post/Controller/FinishGoalVC.swift
@@ -0,0 +1,68 @@
+//
+//  FinishGoalVC.swift
+//  goal post
+//
+//  Created by LinuxPlus on 1/26/18.
+//  Copyright © 2018 ARC. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class FinishGoalVC: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var pointTextField: UITextField!
+    @IBOutlet weak var createGoalBtn: UIButton!
+
+    //Variables
+    var goalDescription: String!
+    var goalType: GoalType!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pointTextField.delegate = self
+        createGoalBtn.bindToKeyboard()
+    }
+
+    func initData(description: String, type: GoalType)  {
+        goalDescription = description
+        goalType = type
+    }
+    
+
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismissDetail()
+    }
+    
+    
+    @IBAction func createGoalPressed(_ sender: Any) {
+        //Pass data to Core Data Model
+        if pointTextField.text != nil   {
+            self.save { (complete) in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+    
+    }
+    
+    func save(completion: (_ finished: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+            //instance of goal
+        let goal = Goal(context: managedContext)
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do  {
+            try managedContext.save()
+            completion(true)
+        }catch {
+            debugPrint("Clound not save: \(error.localizedDescription)")
+            completion(false)
+        }
+    }
+    
+}
