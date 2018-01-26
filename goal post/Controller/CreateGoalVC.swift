c7861e06759f82dc1be7af657c7b2ba1d87d3c98
LinuxPlus <linuxplus@linuxpluss-mac.local>
2018-01-26 08:35:24 -0800

    create CreateGoalVC position views in CreateGoalVC UI

---
+++ goal post/Controller/CreateGoalVC.swift
@@ -0,0 +1,37 @@
+//
+//  CreateGoalVC.swift
+//  goal post
+//
+//  Created by LinuxPlus on 1/25/18.
+//  Copyright © 2018 ARC. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func shortTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextBtnPressed(_ sender: Any) {
+    }
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
