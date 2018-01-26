501f2a6132333bb23dab3582b5212515eb068c7b
LinuxPlus <linuxplus@linuxpluss-mac.local>
2018-01-25 19:15:23 -0800

    configure goalCell method implement ui elements

---
+++ goal post/Views/GoalCell.swift
@@ -0,0 +1,27 @@
+//
+//  GoalCell.swift
+//  goal post
+//
+//  Created by LinuxPlus on 1/25/18.
+//  Copyright © 2018 ARC. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+  
+   
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int)  {
+        goalDescriptionLbl.text = description
+        goalTypeLbl.text = type
+        goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+    
+    
+}
