8d130bd1b377498b6b115ea99842e3a613dfee73
LinuxPlus <linuxplus@linuxpluss-mac.local>
2018-01-25 17:44:14 -0800

    Initial Commit

---
+++ goal post/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goal post
+//
+//  Created by LinuxPlus on 1/25/18.
+//  Copyright © 2018 ARC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
